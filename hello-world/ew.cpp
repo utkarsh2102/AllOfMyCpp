f25d66a24d914d1d2f9472c3bf3a7a7b59b7a083
srihari unnikrishnan <srihari.unnikrishnan@gmail.com>
2018-10-31 17:37:10 +0530

    Some real life advice

---
+++ hello-world/ew.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* 
+ * Ew you using CPP. 
+ * You should go ahead with Kotlin or Python.
+ * Or you can code in Vala ( Cause the main stream sucks, right?)
+ * Safe yourself a world of Pain.
+ * */
+int main(){
+	cout << "Hello, Utkarsh";
+	return 0;
+}
